db4fa7c9285616b01ef35bf8dd0445e9392e6d5b
Marvyn95 <marvinluyombya@gmail.com>
2025-07-05 01:10:41 +0300

    update on configuration safety

--- sb/__init__.py
+++ sb/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
 import pymongo
 from flask_bcrypt import Bcrypt
+import json
 
+with open("../config.json") as config_file:
+    config = json.load(config_file)
 
 #initializing app
 app = Flask(__name__)
-app.secret_key = "secret_secret_secret"
-
+app.secret_key = config.get("SECRET_KEY")
 #connecting to mongodb cluster and database
-cluster = pymongo.MongoClient("mongodb://localhost:27017/")
+cluster = pymongo.MongoClient(config.get("CONNECTION_STRING"))
 db = cluster.sh_app
 
 bcrypt = Bcrypt()
